ea7257510f9f8256d3c27d656715abdd91ef20f3
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-13 21:29:20 +0530

    31412

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class RestaurantsInfo(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    
+    def__str__(self):
+        return self.name
