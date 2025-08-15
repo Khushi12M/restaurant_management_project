0b6de8ad2aa4b954e0c39c9058ff78d670cfdd5a
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-15 10:42:01 +0530

    33029

--- templates/views.py
+++ templates/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from datetime import datetime
 
-def homepage(request):
+from datetime import datetime
+from django.shortcuts import render
+  
+def home(request):
     return render(request, 'homepage.html', {'year': datetime.now().year})
