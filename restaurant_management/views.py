ec2c9cc6d973b08524dc88b2273afc3db97678bd
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-17 19:51:08 +0530

    34638

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,13 +1,17 @@
-from django.conf import settings
-from django.shortcuts import render
- def home(request):
-    context ={
-       'restuarant_name': getattr(setting, 'RESTAURANT_NAME', 'My Restaurant')
-       
+from rest_framework.decorators import api_view
+from rest_framework.response import response
 
-       }
+def restaurant_menu(request):
 
-       return render(request,'home.html', context)
+    Menu =[
+        {"name": "Paneer Butter Masala","description": "Creamy curry with paneer cubes", "price":250.00},
+        {"name":" Chiken Biryani", "description": "Fragrant basmati rice with spiced chicken", "price": 300.00},
+        {"name": "Veg Manchurian", "description": "Crispy vegetable balls in tangy sauce", "price": 220.00},
+        {"name": "Masala Dosa", "description": "Crispy dosa stuffed with spiced potatoes", "prices": 150.00}
+
+
+    ]
+    return Response(Menu)
        
         
 
