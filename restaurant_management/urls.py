6290edd6e7002a68c73686de9fab7447fa03769b
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-17 19:54:16 +0530

    34638

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from. views import restaurant_menu
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('home.urls')),
-    path('api/accounts/',include('account.urls')),
-    path('api/products/',include('products.urls')),
-    path('api/orders/',include('orders.urls')),
+urlpatterns=[
+    path('restaurant/menu/', restaurant_menu, name='restaurant_menu')
 ]
+ 
