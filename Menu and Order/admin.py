9b59460aaafde9c1ca1c845138522927e78009ea
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-18 21:45:30 +0530

    35469

--- Menu and Order/admin.py
+++ Menu and Order/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
-from .models import MenuItem, order
+from.models import RestaurantLocation
 
-class MenuItemAdmin(admin.ModelAdmin):
-    list_dispaly=("id", "name", "price", "is_available", "restaurant", "created_at")
-    list_filter =("is_available", "restaurant")
-    search_fields =("name", "description")
-
-    class OrderAdmin(admin.ModelAdmin):
-        list_dispaly =("id", "restaurant", "customer_name", "status", "created_at")
-        list_filter =("status", "restaurant")
-        search_fields =("customer_name", "restaurant_name")
+admin.site.register(RestaurantLocation)
