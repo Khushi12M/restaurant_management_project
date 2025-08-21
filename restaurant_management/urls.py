e3d317d9148f16c4a5a7f3d5617d2b882767b125
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-21 21:28:48 +0530

    36671

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -1,7 +1,12 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns[
     path('', include('home.urls')),
+    path('menu/', include('Menu and order.urls')),
 ]
+
+if settings.DEBUG:
+    urlpattern+= static(settings.MEDIA_URL,document_root=settins.MEDIA_ROOT)
 
