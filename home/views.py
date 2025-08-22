675969774338f8b9cd2e695a596eae87ba567df2
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-22 21:30:59 +0530

    36669

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from Menu_and_Order.models import MenuItem
 
-# Create your views here.
+def home_view(request):
+    query= request.GET.get('q')
+    if query:
+        menu_items =MenuItem.objects.filter(name_icontains=query)
+        else:
+            menu_items= MenuItem.objects.all()
+
+            return render(request,'home.html',{'menu_items': menu_items,'query': query})
