d6f6901905e4eab65b11b11c2b8f8ae1bc0ead9e
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-23 21:47:42 +0530

    37940

--- home/views.py
+++ home/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from Menu_and_Order.models import MenuItem
 
-def home_view(request):
-    query= request.GET.get('q')
-    if query:
-        menu_items =MenuItem.objects.filter(name_icontains=query)
-        else:
-            menu_items= MenuItem.objects.all()
-
-            return render(request,'home.html',{'menu_items': menu_items,'query': query})
+def faq(request):
+    return   render(request,"faq.html")
