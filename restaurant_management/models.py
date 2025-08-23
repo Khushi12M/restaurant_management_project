68c45dd4114d95f38cf5595a79cdf96a5ba73d34
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-23 21:58:39 +0530

    37938

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-import json
+
 
 class Restaurant(models.Model):
-     address = models.CharField(max_length=255)
-     city = models.CharField(max_length=100)
-     state = models.CharField(max_length=100)
-    zip_code models.CharField(max_length=20)
+     name = models.CharField(max_length=255)
+     phone_number= models.CharField(max_length=100)
+     
 
-    opening_hours = models.JSONField(default=dict)
+    
 
     def_str_(self):
         return self.name
