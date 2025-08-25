46e950c8cc991aa38a0cd3ec05d86e7304471389
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-25 21:51:42 +0530

    38649

--- Menu and Order/models.py
+++ Menu and Order/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 class MenuItem(models.Model):
     name= models.CharField(max_length=100)
-    description= models.TextField()
     price models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/',blank=True, null=True)
+    
 
     def_str_(self):
         return self.name
