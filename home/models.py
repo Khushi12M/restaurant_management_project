daee2aee241331745df49420925f7574075e6801
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-25 21:40:45 +0530

    38651

--- home/models.py
+++ home/models.py
@@ -1,7 +1,9 @@
 from django. db import models
-class RestaurantInfo(models.Model):
-    name= models.CharField(max_length=100, default="Our Restaurant")
-    phone = models.CharField(max_length=20)
+class Feedback(models.Model):
+    name= models.CharField(max_length=100)
+    feedback_text= models.TextField()
+    submitted_at = models.DateField(auto_now_add=True)
+  
 
     def_str_(self):
         return self.name
