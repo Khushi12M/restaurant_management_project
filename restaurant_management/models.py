39b0228a23153e03d25d1cee5c5002a0dd1fd146
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-25 21:46:11 +0530

    38650

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class RestaurantInfo(models.Model):
-     name = models.CharField(max_length=255)
-     history= models.TextField()
-     mission = models.TextField()
+class AboutUs(models.Model):
+     title= models.CharField(max_length=255, default="About Our Restaurant")
+     description = models.TextField()
+      image = models.TextField(upload_to='about_us/',blank=True, null=True)
     
 
     
 
     def_str_(self):
-        return self.name
+        return self.title
        
