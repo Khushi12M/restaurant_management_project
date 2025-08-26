1811e86d1a62230bdf0cf262a6448514bd0855de
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-26 21:42:10 +0530

    39246

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from .models import OpeningHour
+from .models import ContactForm
 
-def home(request):
+def contact_view(request):
+    if request.method =="POST"
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message = form.cleaned_data['message']
+{}
+        print(f"New messgae from {name} ("{email}): {message}")
 
-    hours = OpeningHour.objects.all()
-    return render(request."home/home.html",{"hours": hours})
+ 
+    return render(request."home/contact_success.html",{"name": name})
+    else:
+        form = ContactForm()
+        return render(request. "home/contact.html", {"form": form})
