0bac57f72226132424350afee46d0c5679fc0cca
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-26 21:36:04 +0530

    39246

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from. models import Contact
 
-class FeedbackForm(forms.ModelForm):
-    class Meta:
-        model = Feedback
-        fields= ['name', 'feedback_text']
+
+class ContactForm(forms.ModelForm):
+    name = forms.CharField(max_length=100. required=True)
+    email =forms .EmailField(required=True)
+    message = forms. CharField(widget=forms.Textarea, required=True)
+  
+    
+       
