112fe32fb2e5e3b4ac7791ec0e9af39a1cc03614
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-27 09:29:09 +0530

    39244

--- home/views.py
+++ home/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from .models import ContactForm
+from  restaurant_management .models import RestaurantInfo
 
 def contact_view(request):
-    if request.method =="POST"
-    form = ContactForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('thank_you')
-
-    else:
-        form = ContactForm()
-        return render(request. "contact.html", {"form": form})
-
-        def thank_you(request):
-            return render(request,'thank_you.html')
+    restaurant = RestaurantInfo.objects.first()
+    return render(request,'contact.html',{'restaurant: restaurant'})
+   
