c4958b396449b701c61ba5d5d1dd7fe6046662d5
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-27 09:45:26 +0530

    39682

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-class OpeningHour(models.Model):
-     day = models.CharField(max_length=20)
-     open_time = models.TextField()
-    close_time = models.TextField()
-    
+class RestaurantInfo(models.Model):
+     name = models.CharField(max_length=20)
+    address = models.TextField()
+    phone = models.CharField(max_length=20, blank=true, null=true)
+    email= models.EmailField(blank=True, null=True)
 
     
-
     def_str_(self):
-        return f" {self.day}: {self.open_time.strftime('%I:%M %P)}
+        return self.name
        
