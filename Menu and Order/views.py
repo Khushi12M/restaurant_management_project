c5fb616e247976ac04169e3a15838ab18963df7f
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-28 21:33:59 +0530

    39681

--- Menu and Order/views.py
+++ Menu and Order/views.py
@@ -1,6 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import MenuItem
 
-def menu_page(request):
-    menu_items= MenuItem.objects.all().order_by('category', 'name')
+def menu_view(request):
+    menu_items= MenuItem.objects.all()
+    paginator= Paginator(menu_items,5)
+
+    page_number= request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,'menu.html',{'page_obj': page_obj})
     
