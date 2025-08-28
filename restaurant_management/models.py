2cdd76a8de6d71f5862a596efc7aa12dc57afba9
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-28 21:52:15 +0530

    39679

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
-class RestaurantInfo(models.Model):
+class Chef(models.Model):
      name = models.CharField(max_length=20)
-    address = models.TextField()
+     bio = models.TextField()
     phone = models.CharField(max_length=20, blank=true, null=true)
-    email= models.EmailField(blank=True, null=True)
+   
 
     
     def_str_(self):
