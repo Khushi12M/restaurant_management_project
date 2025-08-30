1faca5fd78040424b664299c13e625e8d07e700c
Khushi Mehra <95957893+Khushi12M@users.noreply.github.com>
2025-08-30 21:21:56 +0530

    40562

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.urls import render
 from .import views
 
 urlpatterns[
     path('contact/',views.contact_view. name='contact'),
-    path('thank_you', views.thank_you_view, name='thank_you'),
+   
 ]
 
 
